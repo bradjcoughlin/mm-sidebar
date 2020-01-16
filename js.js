c787a1843abdc46636b4eaa9c64d3b28dab5d51f
Brad Coughlin <brad@bradcoughlin.com>
2020-01-16 15:17:21 -0800

    wip

--- js.js
+++ js.js
@@ -1,25 +1,3 @@
-let dragSources = document.querySelectorAll('[draggable="true"]');
-dragSources.forEach(dragSource => {
-    dragSource.addEventListener("dragstart", dragStart);
-    dragSource.addEventListener("dragend", dragEnd);
-});
-
-/* TODO: adding inline styles breaks collapsed styles :( */
-function dragStart(e) {
-    this.classList.add("dragging");
-    this.style.backgroundColor = '#0d4896';
-    this.style.border = '2px dashed #BCCDE7';
-    this.style.opacity = '0.4';
-}
-
-function dragEnd(e) {
-    this.classList.remove("dragging");
-    this.style.backgroundColor = 'inherit';
-    this.style.opacity = 'inherit';
-    this.style.border = 'solid 2px transparent';
-}
-
-
 // expand/collapse
 let collapseSources = document.querySelectorAll('.NavGroupToggle');
 collapseSources.forEach(collapseSource => {
